9f77c4e0f2137f979003a4d4b923e54f7545dc0e
dumdoo <rohailfaisal14@gmail.com>
2022-08-15 23:14:11 +0500

    Add monday and others mf

--- app.py
+++ app.py
@@ -2,29 +2,39 @@ from datetime import datetime
 from lesson import Lesson
 import json
 from zoneinfo import ZoneInfo
+from ast import literal_eval
 from flask import *
 
 
 app = Flask(__name__)
 
+
 def get_current_dt() -> datetime:
-    pkt = ZoneInfo('Asia/Karachi')
+    pkt = ZoneInfo("Asia/Karachi")
     return datetime.now(pkt)
-    
-@app.route('/set_subjects', methods=['POST', 'GET'])
+
+
+@app.route("/set_subjects", methods=["POST", "GET"])
 def set_subjects():
-    if request.method == 'GET':
-        return render_template('set_subjects.html')
-    resp = make_response(redirect(url_for('index')))
-    resp.set_cookie('subjects', json.dumps(dict(request.form)))
+    if request.method == "GET":
+        return render_template("set_subjects.html")
+    resp = make_response(redirect(url_for("index")))
+    resp.set_cookie("subjects", json.dumps(dict(request.form)))
     return resp
 
-@app.route('/')
+
+@app.route("/")
 def index():
     # Check if subject cookie has been set
-    if not request.cookies.get('subjects'):
-        return redirect(url_for('set_subjects'))
-    
-    lessons = []
-    resp = make_response(render_template('index.html', lessons=lessons))
-    return resp 
+    if not request.cookies.get("subjects"):
+        return redirect(url_for("set_subjects"))
+
+    lessons = Lesson.get_lessons(
+        get_current_dt(),
+        literal_eval(request.cookies["subjects"])["section"],
+        [
+            literal_eval(request.cookies["subjects"])[opt]
+            for opt in ["OPT-A", "OPT-B", "OPT-C", "OPT-D"]
+        ],
+    )
+    return render_template("index.html", lessons=lessons)
--- lesson.py
+++ lesson.py
@@ -1,6 +1,38 @@
+from __future__ import annotations
 from dataclasses import dataclass
+import json
+from datetime import datetime
 
-@dataclass(frozen=True)
+
+@dataclass
 class Lesson:
     name: str
     location: str
+
+    @classmethod
+    def get_lessons(cls, dt: datetime, section: str, optionals: list[str]) -> list[Lesson]:
+        if len(optionals) != 4:
+            raise ValueError("All four optionals must be specified")
+        if section.upper() not in "KGSTLRMW":
+            raise ValueError(f"Invalid section {section}")
+
+        day = dt.strftime("%a").upper()
+        if day in ["SAT", "SUN"]:
+            day = "MON"
+
+        with open("data.json", encoding="utf-8") as f:
+            data = json.load(f)
+
+        lessons = data[day]["lesson-order"]
+
+        for i, lesson in enumerate(data[day]["comp-lessons"]):
+            if lesson is None:
+                continue
+            lessons[i] = lesson[section]
+
+        for i, optional in enumerate(["OPT-A", "OPT-B", "OPT-C", "OPT-D"]):
+            if data[day][optional] is not None:
+                ii = lessons.index(optional)
+                lessons[ii] = [optionals[i], data[day][optional][optionals[i]]]
+
+        return [cls(*lesson) for lesson in lessons]
